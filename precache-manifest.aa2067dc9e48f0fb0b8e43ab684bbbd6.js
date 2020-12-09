be3f4bf2354f01d78193cc19947ed916091f07b3
Sandip Das <sandeepdas.0519@gmail.com>
2020-12-09 16:13:24 +0530

    Updates

---
+++ precache-manifest.aa2067dc9e48f0fb0b8e43ab684bbbd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32da4e70dc1b3e36007284acaf09a572",
+    "url": "/youtubevideos/index.html"
+  },
+  {
+    "revision": "f2949d0e087174c761f4",
+    "url": "/youtubevideos/static/css/main.d41bcc0d.chunk.css"
+  },
+  {
+    "revision": "d515765a05c4e30b4b31",
+    "url": "/youtubevideos/static/js/2.e34008a6.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtubevideos/static/js/2.e34008a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2949d0e087174c761f4",
+    "url": "/youtubevideos/static/js/main.a1c04176.chunk.js"
+  },
+  {
+    "revision": "99cb56826f09ba0bd705",
+    "url": "/youtubevideos/static/js/runtime-main.91d07e25.js"
+  }
+]);
